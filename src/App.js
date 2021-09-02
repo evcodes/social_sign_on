93640faf306b7fb22953856c7c0288e24e1f9756
evcodes <eddvar@amazon.com>
2021-09-02 12:13:04 -0400

    adding the old version of aws-amplify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify, { Auth, Hub } from "aws-amplify";
+import awsconfig from "./aws-exports";
+import "./App.css";
+Amplify.configure(awsconfig);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = { user: null, customState: null };
+
+  componentDidMount() {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          console.log(data);
+          this.setState({ user: data });
+          break;
+        case "signOut":
+          this.setState({ user: null });
+          break;
+        case "customOAuthState":
+          this.setState({ customState: data });
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then((user) => this.setState({ user }))
+      .catch(() => console.log("Not signed in"));
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          style={
+            user == null
+              ? { backgroundColor: "#4285f4", color: "white" }
+              : { display: "none" }
+          }
+          onClick={() => Auth.federatedSignIn({ provider: "Google" })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Login with Google
+        </button>
+        <button
+          style={
+            user
+              ? { backgroundColor: "red", color: "white" }
+              : { display: "none" }
+          }
+          onClick={() => Auth.signOut()}
+        >
+          Sign Out: {user ? user.username : "no user"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
